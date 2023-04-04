0441157868f1d724d256416a3cc1150107263c98
mynameisseohyeon <uimi3606@gmail.com>
2023-04-04 20:39:41 +0900

    Design

---
+++ Desktop/이서현/학교/동아리/frontend2-javascript/js/modal.js
@@ -0,0 +1,5 @@
+const closeBtn = modal.querySelector(".close-modal-btn"); //모달 창 닫기 버튼
+closeBtn.addEventListener("click", (e) => {
+  modal.style.display = "none"; //닫기 버튼 클릭 시 화면에서 안보이게 된다
+  overlay.style.display = "none";
+});
