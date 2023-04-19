5bcfff503600169972eef0dc07373baa87ae1492
mynameisseohyeon <uimi3606@gmail.com>
2023-04-20 03:54:24 +0900

    DESIGN 버튼 클릭 시 오디오 재생

--- js/startPage.js
+++ js/startPage.js
@@ -1,25 +1,34 @@
-let outsideView = document.querySelector(".outside-view");
-let num = 0;
+// let outside = document.querySelector(".start-page-background"); /*div */
+// let outsideView = document.querySelector(".outside-view"); /*ul */
+// let num = 0;
 
-$(document).ready(function () {
-  setInterval(function () {
-    if (num < 2) {
-      outsideView.animate(
-        {
-          left: "-=1200",
-        },
-        "slow"
-      );
-      num++;
-    } else {
-      num = 0;
+// document.addEventListener("DOMContentLoaded", function () {
+//   setInterval(function () {
+//     if (num < 2) {
+//       outsideView.animate(
+//         {
+//           left: "-=1200",
+//         },
+//         1000 /* duration in milliseconds */
+//       );
+//       num++;
+//     } else {
+//       num = 0;
 
-      outsideView.animate(
-        {
-          left: 0,
-        },
-        "slow"
-      );
-    }
-  }, 3000);
+//       outsideView.animate(
+//         {
+//           left: 0,
+//         },
+//         1000 /* duration in milliseconds */
+//       );
+//     }
+//   }, 3000);
+// });
+
+const participationBtn = document.querySelector(".participation-btn"); //축제 참여 버튼튼
+const audio = document.querySelector(".audio"); //기차 달리는 bgm
+
+participationBtn.addEventListener("click", () => {
+  participationBtn.style.opacity = 0; // 페이드아웃 되도록 opacity 값 변경
+  audio.play(); //기차 bgm 재생
 });
