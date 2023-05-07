0c1add1de49efc64c4e2021dc1aca36a7f43d128
getsoss <getsoss@naver.com>
2023-05-07 20:02:17 +0900

    searchCommit

--- js/search.js
+++ js/search.js
@@ -54,41 +54,12 @@ function searchEvt() {
   for (var i = 0; i < elements.length; i++) {
     elements[i].remove();
   }
+
   elementHidden("map");
   elementVisible("searchBox");
 
   var searchContainer = document.getElementById("searchContainer"); // 검색 내용에 해당되는 요소를 담을 searchContainer
   var searchInput = document.getElementById("searchInput"); // 검색 내용을 찾을 searchInput
-
-  // class SearchContainer extends HTMLElement {
-  //   //상세창에 이미지 넣기
-  //   constructor(dataIndex, city) {
-  //     super();
-  //     this.dataIndex = dataIndex;
-  //     this.city = city;
-  //   }
-
-  //   setCity(city) {
-  //     this.city = city;
-  //   }
-
-  //   connectedCallback() {
-  //     const searchContainer = this.querySelector(".searchontainer");
-  //     searchContainer.setAttribute(
-  //       "src",
-  //       `./img/축제사진/${this.city}/${this.dataIndex}.jpg`
-  //     ); //각각의 이미지
-  //   }
-  // }
-  // window.onload = function () {
-  //   //지도 api
-  //   var container = document.getElementById("searchContainerMap");
-  //   var options = {
-  //     center: new kakao.maps.LatLng(33.450701, 126.570667),
-  //     level: 3,
-  //   };
-  //   var map = new kakao.maps.Map(container, options);
-  // };
 
   for (var i = 0; i < festivalDataList.Busan.length; i++) {
     // 검색 내용이 해당되는 festivalList 있는 지 찾기 위한 반복문
@@ -96,8 +67,21 @@ function searchEvt() {
       if (festivalDataList.Busan[i][a].includes(searchInput.value)) {
         // 검색 내용이 포함되었다면 searchBox에 해당 축제들의 타이틀들을 추가
         const festivalText = document.createElement("h1");
+        const festivalAreaIns = document.createElement("h3");
+        const festivalDate = document.createElement("h3");
+        const festivalMainIns = document.createElement("h3");
+        const festivalTag = document.createElement("h3");
+        const festivalSite = document.createElement("h3");
+        const festivalImage = document.createElement("img");
+        festivalImage.src = "./img/축제사진/Busan/" + i + ".jpg";
 
         festivalText.innerHTML = festivalDataList.Busan[i].festivalTitle;
+        festivalAreaIns.innerHTML = festivalDataList.Busan[i].festivalAreaIns;
+        festivalDate.innerHTML = festivalDataList.Busan[i].festivalDate;
+        festivalMainIns.innerHTML = festivalDataList.Busan[i].festivalMainIns;
+        festivalTag.innerHTML = festivalDataList.Busan[i].festivalTag;
+        festivalSite.innerHTML = festivalDataList.Busan[i].festivalSite;
+
         searchBox.appendChild(festivalText);
         festivalText.addEventListener("click", function () {
           // Ÿ��Ʋ�� �˻��ϸ� ������ �ִ� searchContainer�� ���̰� �ǰ� ��� Ÿ��Ʋ���� ������
@@ -107,11 +91,27 @@ function searchEvt() {
             elements[i].style.transition = "0s";
             elements[i].style.visibility = "hidden";
           }
+          var elements3 = document.querySelectorAll("h3"); // 이전에 있던 모든 h3 제거.
+          for (var i = 0; i < elements3.length; i++) {
+            elements3[i].remove();
+          }
           var clickTitle = document.querySelectorAll("h2"); // 이전에 있던 건 제거
           clickTitle[0].remove();
+
+          var festivalimg = document.querySelectorAll("img"); // 이전에 있던 건 제거
+          for (var i = 0; i < festivalimg.length; i++) {
+            festivalimg[i].remove();
+          }
+
           const festivalText2 = document.createElement("h2"); // Ŭ���� Ÿ��Ʋ�� searchContainer�� �߰��ϱ�
           festivalText2.innerHTML = festivalText.innerHTML;
           searchContainer.appendChild(festivalText2);
+          searchContainer.appendChild(festivalAreaIns);
+          searchContainer.appendChild(festivalDate);
+          searchContainer.appendChild(festivalMainIns);
+          searchContainer.appendChild(festivalTag);
+          searchContainer.appendChild(festivalSite);
+          searchContainer.appendChild(festivalImage);
         });
         break;
       }
