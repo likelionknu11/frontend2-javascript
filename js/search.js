7c4c05aac8b7f3e462f22038c7b190f578346b36
getsoss <getsoss@naver.com>
2023-05-08 21:26:10 +0900

    kakaoMapTest

--- js/search.js
+++ js/search.js
@@ -53,14 +53,6 @@ searchSpanCloseBtn.addEventListener("click", (event) => {
   }
 });
 
-var container = document.getElementById("searchContainerMap"); //지도 api가져오기
-var options = {
-  center: new kakao.maps.LatLng(33.450701, 126.570667),
-  level: 3,
-};
-
-var map = new kakao.maps.Map(container, options);
-
 function searchEvt() {
   var searchContainer = document.getElementById("searchContainer");
   if (searchContainer.style.visibility === "visible") {
@@ -141,3 +133,12 @@ function searchEvt() {
     }
   }
 }
+
+var mapContainer = document.getElementById("kakaoMap"), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+  };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
